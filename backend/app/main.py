356ce0311077bb64882de2b3789cd252a02db001
David Kornblit <davidkor@std.hac.ac.il>
2026-02-17 17:39:33 +0200

    Fixed path

--- backend/app/main.py
+++ backend/app/main.py
@@ -24,7 +24,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=os.getenv("ALLOWED_ORIGINS", "http://localhost:3000").split(","),
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
